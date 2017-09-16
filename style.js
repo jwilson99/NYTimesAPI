01c59a4843aff0d8a06dfa9267339dd46136740e
Gene Williams <GeneWillb@Gmail.com>
2017-09-16 13:15:44 -0400

    ajax call

---
+++ style.js
@@ -0,0 +1,22 @@
+var APIKey = "75d2df1262dd4f54bd6f8ab946d7f9f6";
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key="+ APIKey + "&q=" + search + "&begin_date=" + startYear + "&end_date=" + endYear;
+
+var search;
+
+var startYear;
+
+var endYear;
+
+var limit;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).done(function(response) {
+   console.log(response);
+   
+   var result = response.response;
+   console.log(result);
+});
+
